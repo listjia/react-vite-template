5e01015fdd96cd2d360eb34ee59f4bf932a1272a
listjia <jiachen2023@outlook.com>
2025-01-04 01:25:34 +0800

    1

--- RAS3.1-React18-TypeScript/Ras3.1/src/vite-env.d.ts
+++ RAS3.1-React18-TypeScript/Ras3.1/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_SERVER_URL: string; // 明确声明 VITE_SERVER_URL 的类型
+    // 可以继续添加其他环境变量
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
