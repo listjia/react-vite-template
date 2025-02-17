624e62cf3738ba2592585a832d9da797e13b5a6e
listjia <jiachen2023@outlook.com>
2025-02-17 13:16:07 +0800

    1

---
+++ RAS3.1-Vue3.5-JavaScript/RAS3.1-Vue3.5-JavaScript/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="text-red-400 text-5xl  ">
+   ddddddddddddddddddddddd
+
+   
+  </div>
+  <v-btn color="orange" >
+
+</v-btn>
+</template>
+
+<style scoped>
+/* .custom-button {
+  background: blue;
+  color: white;
+} */
+</style>
---
+++ RAS3.1_Vue3.5_TypeScript/pure-admin-thin-i18n/src/views/welcome/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineOptions({
+  name: "Welcome"
+});
+</script>
+
+<template>
+  <h1 class="text-red-500">Pure-Admin-Thin（国际化版本）</h1>
+</template>
